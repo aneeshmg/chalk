2efed8e78fe59f489c0c1301c9bbb6c966dcb4c2
Aneesh M Gangadhar <aneesh.mg2007@gmail.com>
2018-04-09 17:21:25 +0530

    enable compression/min [ci skip]

---
+++ assets/application-923b9c2c32bb489472729795d3d8bfcad64a88a57b124426a553a3ecc2a6e842.js
@@ -0,0 +1,3 @@
+$(function(){"function"==typeof $.fn.scrollAppear&&$(".scrollappear").scrollAppear(),$(".fluidbox-trigger").fluidbox(),$(".article-share a").on("click",function(){return window.open($(this).attr("href"),"Share","width=200,height=200,noopener"),!1})});
+//# sourceMappingURL=/chalk/assets/source-maps/application.js.map
+//# sourceURL=_assets/javascripts/application.js
